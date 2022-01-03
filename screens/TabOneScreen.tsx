70005329aab8405f578ebc536ae4f5482db0371d
JaiRaga <raga.j.santhosh@gmail.com>
2022-01-03 17:56:08 +0530

    :zap: passed chatRoom props to ChatRoomItem component

--- screens/TabOneScreen.tsx
+++ screens/TabOneScreen.tsx
@@ -1,56 +1,24 @@
 import * as React from 'react'
 import { View, Image, Text, StyleSheet } from 'react-native'
 
+import ChatRoomItem from '../components/ChatRoomItem'
+import chatRoomData from '../assets/dummy-data/ChatRooms'
+
+const chatRoom1 = chatRoomData[0]
+const chatRoom2 = chatRoomData[2]
+
 export default function TabOneScreen() {
 	return (
-		<View style={styles.container}>
-			<Image
-				source={{ uri: 'https://avatars.githubusercontent.com/u/44367062?v=4' }}
-				style={styles.image}
-			/>
-			<View style={styles.rightContainer}>
-				<View style={styles.details}>
-					<Text style={styles.name}>Raga Jai Santhosh</Text>
-					<Text style={styles.grayedText}>1:00 AM</Text>
-				</View>
-				<Text numberOfLines={1} style={styles.grayedText}>
-					Lorem ipsum dolor sit amet consectetur adipisicing elit. Nulla culpa
-					iste minima sit totam nesciunt praesentium provident perferendis in
-					sapiente! Nostrum ex, labore est delectus animi quidem maiores
-					repudiandae tenetur.
-				</Text>
-			</View>
+		<View style={styles.page}>
+			<ChatRoomItem chatRoom={chatRoom1} />
+			<ChatRoomItem chatRoom={chatRoom2} />
 		</View>
 	)
 }
 
 const styles = StyleSheet.create({
-	container: {
-		display: 'flex',
-		flexDirection: 'row',
-		padding: 10,
-	},
-	image: {
-		height: 50,
-		width: 50,
-		borderRadius: 50,
-		marginRight: 10,
-	},
-	rightContainer: {
+	page: {
 		flex: 1,
-		justifyContent: 'center',
-	},
-	details: {
-		flexDirection: 'row',
-		justifyContent: 'space-between',
-	},
-	name: {
-		fontWeight: 'bold',
-		fontSize: 18,
-		marginBottom: 3,
-	},
-	grayedText: {
-		color: 'grey',
-		fontWeight: '300',
+		backgroundColor: 'white',
 	},
 })
